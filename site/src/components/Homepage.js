fa30f34055b1b9c86606d6a8c0feff99bafb1f14
Marek Klofac <marek.klofac@cz.ibm.com>
2023-05-13 19:34:09 +0200

    basic menu structure done

--- site/src/components/Homepage.js
+++ site/src/components/Homepage.js
@@ -3,41 +3,17 @@ import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import { calloutLink } from './Homepage.module.scss';
 
-import Carbon from './carbon.jpg';
+import Carbon from './mainpage.png';
 
-const FirstLeftText = () => <p>Callout component</p>;
+const FirstLeftText = () => <p>IBM QRadar SIEM</p>;
 
 const FirstRightText = () => (
   <p>
-    This is a callout component. You can edit the contents by updating the{' '}
-    <a href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/5fe12de31bb19fbfa2cab7c69cd942f55aa06f79/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
-      pre-shadowed homepage template
-    </a>
-    . You can also provide <code>color</code> and <code>backgroundColor</code>{' '}
-    props to suit your theme.
-    <a
-      className={calloutLink}
-      href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/main/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
-      Homepage source →
-    </a>
+    This page provides all technical content and step-by-step guides for IBM QRadar SIEM functionalities.
   </p>
 );
 
-const SecondLeftText = () => <p>Callout component</p>;
-
-const SecondRightText = () => (
-  <p>
-    You can also not use these components at all by not providing the callout
-    props to the template or writing your own template.
-    <a
-      className={calloutLink}
-      href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/main/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
-      Homepage source →
-    </a>
-  </p>
-);
-
-const BannerText = () => <h1>Carbon Gatsby theme</h1>;
+const BannerText = () => <h1>IBM QRadar SIEM hands-on labs</h1>;
 
 const customProps = {
   Banner: <HomepageBanner renderText={BannerText} image={Carbon} />,
@@ -49,7 +25,8 @@ const customProps = {
       rightText={FirstRightText}
     />
   ),
-  SecondCallout: (
+/*
+    SecondCallout: (
     <HomepageCallout
       leftText={SecondLeftText}
       rightText={SecondRightText}
@@ -57,6 +34,7 @@ const customProps = {
       backgroundColor="#061f80"
     />
   ),
+*/
 };
 
 // spreading the original props gives us props.children (mdx content)
